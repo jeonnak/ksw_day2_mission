acc892c21d1154c42265b43706a9fef116c3e071
JNK <deal6789@naver.com>
2023-01-13 16:56:32 +0900

    second mission ver.2

--- day02_mission.py
+++ day02_mission.py
@@ -6,8 +6,11 @@ if, else 문을 사용하여 작은 것(small)과 녹색(green)을
 (예: 체리는 작고 녹색이 아니다, 완두콩은 작고 녹색이다, 수박은 크고 녹색이다,
 호박은 크고 녹색이 아니다).
 """
-small = True
-green = True
+
+import random
+
+small = random.choice([True, False])
+green = random.choice([True, False])
 
 if small:
     if green:
